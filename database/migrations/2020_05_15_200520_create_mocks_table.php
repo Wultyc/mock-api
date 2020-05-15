0d88402e8e47ee79cb17a491f1cf8874c1b31ac8
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2020-05-15 21:40:16 +0100

    Mock Migration

---
+++ database/migrations/2020_05_15_200520_create_mocks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mocks', function (Blueprint $table) {
+            $table->id();
+            $table->string("endpoint");
+            $table->text("response");
+            $table->timeStamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mocks');
+    }
+}
