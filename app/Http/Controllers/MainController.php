ca7b1039d7b6ca770cbfa374657a276e2e6dab03
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2020-05-16 00:09:38 +0100

    Main controller

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function index() {
+        return view('welcome');
+    }
+}
