cc561fee97f9cffb67a233a87b60322f994d2945
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2020-05-15 23:26:05 +0100

    MockResource Controller and Resource

--- app/Http/Resources/MockResource.php
+++ app/Http/Resources/MockResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class mock-resource extends JsonResource
+class MockResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
