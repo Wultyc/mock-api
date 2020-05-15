d8f5a9f73dee657c0dc7f80026286282a8f44cac
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2020-05-16 00:09:20 +0100

    Mock Resource Controller with methood base

--- app/Http/Controllers/MockResourceController.php
+++ app/Http/Controllers/MockResourceController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Mock;
 
 class MockResourceController extends Controller
 {
-    //
+    public function get($endpoint){
+
+    }
+
+    public function post($endpoint){
+
+    }
+
+    public function put($endpoint){
+
+    }
+    
+    public function delete($endpoint){
+
+    }
 }
