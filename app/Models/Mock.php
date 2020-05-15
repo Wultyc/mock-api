4a2015853968e7e410864acad178a5826af13a2f
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2020-05-16 00:55:55 +0100

    Fixing Soft Delete dependencies

--- app/Models/Mock.php
+++ app/Models/Mock.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Mock extends Model
 {
     use SoftDeletes;
 
-
+    protected $fillable = ['endpoint', 'query', 'payload'];
 }
