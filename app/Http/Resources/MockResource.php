9d49968836105e17fb586a12b580e6bc624259ef
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2020-05-16 12:30:52 +0100

    Mock Resource

--- app/Http/Resources/MockResource.php
+++ app/Http/Resources/MockResource.php
@@ -14,6 +14,7 @@ class MockResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        //return parent::toArray($request);
+        return json_decode($this->payload);
     }
 }
