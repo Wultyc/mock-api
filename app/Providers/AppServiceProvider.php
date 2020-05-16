04afffb4a0827b135611fc64a8b477b0ed473cef
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2020-05-16 12:30:31 +0100

    Disable wrapping

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //Remove Wrapping on resources
+        JsonResource::withoutWrapping();
     }
 }
