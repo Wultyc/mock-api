bc59999e2e3cbb6bbc8e145f37276ebcc0af7c2b
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2020-05-16 15:29:12 +0100

    Ranaming resource

--- app/Http/Resources/MockManagementList.php
+++ app/Http/Resources/MockManagementList.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class MockManagement extends JsonResource
+class MockManagementList extends JsonResource
 {
     /**
      * Transform the resource into an array.
