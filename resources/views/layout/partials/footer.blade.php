05b9538bccf38b9c1439f02088971c980abd67f0
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2020-05-16 17:50:49 +0100

    footer message

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -1,3 +1,5 @@
 <div class="container">
-    <span class="text-muted">Place sticky footer content here.</span>
+    <span class="text-muted">
+        This project was developed by J. Gabriel Azevedo also know as Wultyc and its distribuited with <a href="https://github.com/Wultyc/mock-api/blob/master/LICENSE" target="_blank">MIT Licence</a>
+    </span>
 </div>
