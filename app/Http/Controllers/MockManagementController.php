207716cdc4d6bef442fdedfa6a35b1ebd896f8bb
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2020-05-16 15:30:04 +0100

    Insert and updating list resource

--- app/Http/Controllers/MockManagementController.php
+++ app/Http/Controllers/MockManagementController.php
@@ -3,14 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \Illuminate\Database\QueryException;
 use App\Models\Mock;
-use App\Http\Resources\MockManagement;
+use App\Http\Resources\MockManagementList;
+use App\Http\Resources\MockManagementInsert;
 
 class MockManagementController extends Controller
 {
     public function list(Request $request)
     {
         $mocks = Mock::withTrashed()->get();
-        return MockManagement::collection($mocks);
+        return MockManagementList::collection($mocks);
+    }
+
+    public function insert(Request $request)
+    {
+        $mocks = $request->post();
+        $status = null;
+        $reportArray = array();
+
+        foreach($mocks as $mock){
+            $mock_obj = new Mock;
+
+            $mock_obj->endpoint = $mock['endpoint'];
+            $mock_obj->query = $mock['query'];
+            $mock_obj->payload = $mock['payload'];
+
+            try{
+                $status = 'ok';
+                $mock_obj->save();
+            } catch(QueryException $e) {
+                $status = 'fail';
+            }
+
+            array_push($reportArray, array('endpoint'=>$mock['endpoint'], 'status'=>$status));
+        }
+        return MockManagementInsert::collection($reportArray);
     }
 }
