aa3851ec962f1f4a8bf55e333c2d59579c1754b5
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2020-06-07 10:43:53 +0100

    encapsulating management methods

--- app/Http/Controllers/APIs/MockManagementController.php
+++ app/Http/Controllers/APIs/MockManagementController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\APIs;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use \Illuminate\Database\QueryException;
 use App\Models\Mock;
 use App\Http\Resources\MockManagementList;
 use App\Http\Resources\MockManagementInsert;
@@ -19,26 +18,7 @@ class MockManagementController extends Controller
 
     public function insert(Request $request)
     {
-        $mocks = $request->post();
-        $status = null;
-        $reportArray = array();
-
-        foreach($mocks as $mock){
-            $mock_obj = new Mock;
-
-            $mock_obj->endpoint = $mock['endpoint'];
-            $mock_obj->query = $mock['query'];
-            $mock_obj->payload = $mock['payload'];
-
-            try{
-                $status = 'ok';
-                $mock_obj->save();
-            } catch(QueryException $e) {
-                $status = 'fail';
-            }
-
-            array_push($reportArray, array('endpoint'=>$mock['endpoint'], 'status'=>$status));
-        }
+        $reportArray = Mock::storeListEndpoits($request);
         return MockManagementInsert::collection($reportArray);
     }
 }
--- app/Models/Mock.php
+++ app/Models/Mock.php
@@ -4,10 +4,43 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
 
 class Mock extends Model
 {
     use SoftDeletes;
 
     protected $fillable = ['endpoint', 'query', 'payload'];
+
+    /**
+     * Stores many endpoints at once
+     *
+     * @param Request $request
+     * @return array $reportArray
+     */
+    public static function storeListEndpoits(Request $request)
+    {
+        $mocks = $request->post(); //Request payload
+        $status = null;
+        $reportArray = array();
+
+        foreach ($mocks as $mock) {
+            $mock_obj = new Mock;
+
+            $mock_obj->endpoint = $mock['endpoint'];
+            $mock_obj->query = $mock['query'];
+            $mock_obj->payload = $mock['payload'];
+
+            try {
+                $status = 'ok';
+                $mock_obj->save();
+            } catch (QueryException $e) {
+                $status = 'fail';
+            }
+
+            array_push($reportArray, array('endpoint' => $mock['endpoint'], 'status' => $status));
+        }
+        return $reportArray;
+    }
 }
